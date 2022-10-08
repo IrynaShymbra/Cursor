5fcb07b12c53a5fc107d0d839c41385b32ad315d
iry <iry.shymbra@gmail.com>
2022-10-08 20:59:33 +0300

    add new file second.py

---
+++ second.py
@@ -0,0 +1,6 @@
+"""
+    1. Create repository
+    2. Add file "first" to branch master
+    3. Create branch dev add file "second" with some content
+    4. Open pull request from dev to master and attach link to your trello ticket
+"""
